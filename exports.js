5b6222970ac393d9cd69085092c37f21fd18476f
aliasfalse <matej@voboril.org>
2016-07-25 22:18:01 +0000

    porting data from warframe-worldstate-parser

---
+++ exports.js
@@ -0,0 +1,29 @@
+var conclave = require('./data/conclaveData.json');
+var events = require('./data/eventsData.json');
+var factions = require('./data/factionsData.json');
+var fissureModifiers = require('./data/fissureModifiers.json');
+var languages  = require('./data/languages.json');
+var missionTypes = require('./data/missionTypes.json');
+var operationTypes = require('./data/operationTypes.json');
+var persistentEnemy = require('./data/persistentEnemyData.json');
+var solNodes = require('./data/solNodes.json');
+var sortie = require('./data/sortieData.json');
+var syndicates = require('./data/syndicatesData.json');
+var upgradeTypes = require('./data/upgradeTypes.json');
+
+module.exports = {
+  conclave,
+  events,
+  factions,
+  fissureModifiers,
+  languages,
+  missionTypes,
+  languages,
+  missionTypes,
+  operationTypes,
+  persistentEnemy,
+  solNodes,
+  sortie,
+  syndicates,
+  upgradeTypes
+}
