9ec04d1b97c2256f46c55d9db699e5eb92fbd7e4
Tobiah <matej@voboril.org>
2019-01-10 16:21:31 +0000

    cleanups

--- exports.js
+++ exports.js
@@ -1,20 +1,20 @@
-const arcanes = require("./data/arcanes.json");
-const conclave = require("./data/conclaveData.json");
-const events = require("./data/eventsData.json");
-const factions = require("./data/factionsData.json");
-const fissureModifiers = require("./data/fissureModifiers.json");
-const languages  = require("./data/languages.json");
-const missionTypes = require("./data/missionTypes.json");
-const operationTypes = require("./data/operationTypes.json");
-const persistentEnemy = require("./data/persistentEnemyData.json");
-const solNodes = require("./data/solNodes.json");
-const sortie = require("./data/sortieData.json");
-const syndicates = require("./data/syndicatesData.json");
-const tutorials = require("./data/tutorials.json");
-const upgradeTypes = require("./data/upgradeTypes.json");
-const warframes = require("./data/warframes.json");
-const weapons = require("./data/weapons.json");
-const synthTargets = require("./data/synthTargets.json");
+const arcanes = require('./data/arcanes.json');
+const conclave = require('./data/conclaveData.json');
+const events = require('./data/eventsData.json');
+const factions = require('./data/factionsData.json');
+const fissureModifiers = require('./data/fissureModifiers.json');
+const languages = require('./data/languages.json');
+const missionTypes = require('./data/missionTypes.json');
+const operationTypes = require('./data/operationTypes.json');
+const persistentEnemy = require('./data/persistentEnemyData.json');
+const solNodes = require('./data/solNodes.json');
+const sortie = require('./data/sortieData.json');
+const syndicates = require('./data/syndicatesData.json');
+const tutorials = require('./data/tutorials.json');
+const upgradeTypes = require('./data/upgradeTypes.json');
+const warframes = require('./data/warframes.json');
+const weapons = require('./data/weapons.json');
+const synthTargets = require('./data/synthTargets.json');
 
 const enUS = {
   arcanes,
@@ -33,11 +33,11 @@ const enUS = {
   upgradeTypes,
   warframes,
   weapons,
-  synthTargets
+  synthTargets,
 };
 
 module.exports = {
-  "en_US": enUS,
+  en_US: enUS,
   arcanes,
   conclave,
   events,
@@ -54,5 +54,5 @@ module.exports = {
   upgradeTypes,
   warframes,
   weapons,
-  synthTargets
+  synthTargets,
 };
