1a6b82f73055834bc72256d59913b40a6c247351
Matt <7128721+TobiTenno@users.noreply.github.com>
2025-02-05 22:04:53 -0600

    fix: sortLanguages was badly laid out (#599)

--- tools/sortLanguages.js
+++ tools/sortLanguages.js
@@ -1,14 +1,41 @@
-import { readFileSync, writeFileSync } from 'node:fs';
+#!/usr/bin/env node
+
+import { readFile, writeFile } from 'node:fs/promises';
 import { join, dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
+import yargs from 'yargs/yargs';
+
+const options = yargs(process.argv)
+  .usage('Usage -c')
+  .option('c', { alias: 'check', describe: `Check only, don't write file`, type: 'boolean', demandOption: false })
+  .parse();
+
+const checkOnly = await options.c;
+
 const dirName = dirname(fileURLToPath(import.meta.url));
+const sort = (obj) => {
+  return Object.keys(obj)
+    .sort()
+    .reduce(function (result, key) {
+      result[key] = obj[key];
+      return result;
+    }, {});
+};
 
 const dataDir = join(dirName, '..', 'data');
-const data = JSON.parse(readFileSync(join(dataDir, 'languages.json'), 'utf8'));
+const data = JSON.parse(await readFile(join(dataDir, 'languages.json'), 'utf8'));
 
-const sortedData = JSON.stringify(data, Object.keys(data).sort(), 2);
-writeFileSync(join(dataDir, 'languages_sorted.json'), sortedData);
+const sortedData = sort(data);
 
-// eslint-disable-next-line no-console
+if (JSON.stringify(sortedData, undefined, 2) !== JSON.stringify(data, undefined, 2)) {
+  console.error('Data not sorted');
+  if (checkOnly) process.exit(1);
+}
+
+if (checkOnly) process.exit(0);
+
+await writeFile(join(dataDir, 'languages.json.bak'), JSON.stringify(data, undefined, 2));
+await writeFile(join(dataDir, 'languages.json'), JSON.stringify(sortedData, undefined, 2));
+
 console.log('language.json has been sorted!');
