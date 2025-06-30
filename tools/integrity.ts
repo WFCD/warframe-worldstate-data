b7adee993dc9d311dbea3adc987f548938bb59b7
SlayerOrnstein <6075693+SlayerOrnstein@users.noreply.github.com>
2025-06-30 07:59:29 -0400

    fix: expand(?) args

--- tools/integrity.ts
+++ tools/integrity.ts
@@ -3,10 +3,11 @@
  * @param thing to encourage to have data
  * @param args arguments to ensure
  */
-export const insist = (thing: Record<string, unknown>, args: string[]) => {
+export const insist = (thing: Record<string, unknown>, ...args: string[]) => {
   if (!thing || !Object.keys(thing).length) {
     throw new TypeError('The provided data does not have the required properties.');
   }
+
   args.forEach((arg) => {
     if (!thing[arg]) {
       throw new TypeError('The provided data does not have the required properties.');
