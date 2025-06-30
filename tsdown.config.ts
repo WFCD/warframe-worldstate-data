9396cfcb0545c596355e9beb9d4fb3f2970d4a44
SlayerOrnstein <6075693+SlayerOrnstein@users.noreply.github.com>
2025-06-30 07:58:39 -0400

    build: copy data files into dist

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from 'tsdown'
+import { defineConfig } from 'tsdown';
 
 export default defineConfig({
-  entry: ['./tools'],
-})
+  entry: ['exports.ts', 'safeImport.ts', './tools'],
+  copy: ['./data']
+});
