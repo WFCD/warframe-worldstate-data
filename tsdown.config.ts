05d84287005a3f92e858fa3dc584a1d54cc86699
SlayerOrnstein <6075693+SlayerOrnstein@users.noreply.github.com>
2025-06-30 09:01:19 -0400

    build: don't bundle sortlang

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from 'tsdown';
 
 export default defineConfig({
-  entry: ['exports.ts', 'safeImport.ts', './tools'],
-  copy: ['./data']
+  entry: [
+    'exports.ts',
+    'safeImport.ts',
+    './tools/integrity.ts',
+    './tools/timeDate.ts',
+    './tools/translation.ts',
+    './tools/utilities.ts',
+  ],
+  copy: ['./data'],
 });
